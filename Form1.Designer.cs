0b568230985164d73366cf60047eb68f1376b6d0
marcos.afajunio <marcos.afajunio@AME0613379W10-1>
2024-05-08 21:11:23 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace calendario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBoxDatainicial = new System.Windows.Forms.TextBox();
+            this.textBoxDataFinal = new System.Windows.Forms.TextBox();
+            this.textBoxDataAtual = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.btnPegardata = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAutoInicial = new System.Windows.Forms.TextBox();
+            this.textBoxAutoFinal = new System.Windows.Forms.TextBox();
+            this.textBoxAutoHoje = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Restaurar = new System.Windows.Forms.ToolStripMenuItem();
+            this.Mensagem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Fechar = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(348, 26);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBoxDatainicial
+            // 
+            this.textBoxDatainicial.Location = new System.Drawing.Point(472, 79);
+            this.textBoxDatainicial.Name = "textBoxDatainicial";
+            this.textBoxDatainicial.Size = new System.Drawing.Size(130, 26);
+            this.textBoxDatainicial.TabIndex = 1;
+            // 
+            // textBoxDataFinal
+            // 
+            this.textBoxDataFinal.Location = new System.Drawing.Point(472, 117);
+            this.textBoxDataFinal.Name = "textBoxDataFinal";
+            this.textBoxDataFinal.Size = new System.Drawing.Size(130, 26);
+            this.textBoxDataFinal.TabIndex = 2;
+            // 
+            // textBoxDataAtual
+            // 
+            this.textBoxDataAtual.Location = new System.Drawing.Point(472, 154);
+            this.textBoxDataAtual.Name = "textBoxDataAtual";
+            this.textBoxDataAtual.Size = new System.Drawing.Size(130, 26);
+            this.textBoxDataAtual.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(374, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Data inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Data final";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(375, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Data Atual";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 46);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // btnPegardata
+            // 
+            this.btnPegardata.Location = new System.Drawing.Point(363, 197);
+            this.btnPegardata.Name = "btnPegardata";
+            this.btnPegardata.Size = new System.Drawing.Size(239, 49);
+            this.btnPegardata.TabIndex = 8;
+            this.btnPegardata.Text = "selecionar data";
+            this.btnPegardata.UseVisualStyleBackColor = true;
+            this.btnPegardata.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-2, 403);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data Inicial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-1, 451);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Data Final";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-1, 503);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Data Atual";
+            // 
+            // textBoxAutoInicial
+            // 
+            this.textBoxAutoInicial.Location = new System.Drawing.Point(98, 397);
+            this.textBoxAutoInicial.Name = "textBoxAutoInicial";
+            this.textBoxAutoInicial.Size = new System.Drawing.Size(125, 26);
+            this.textBoxAutoInicial.TabIndex = 12;
+            // 
+            // textBoxAutoFinal
+            // 
+            this.textBoxAutoFinal.Location = new System.Drawing.Point(98, 448);
+            this.textBoxAutoFinal.Name = "textBoxAutoFinal";
+            this.textBoxAutoFinal.Size = new System.Drawing.Size(126, 26);
+            this.textBoxAutoFinal.TabIndex = 13;
+            // 
+            // textBoxAutoHoje
+            // 
+            this.textBoxAutoHoje.Location = new System.Drawing.Point(100, 503);
+            this.textBoxAutoHoje.Name = "textBoxAutoHoje";
+            this.textBoxAutoHoje.Size = new System.Drawing.Size(124, 26);
+            this.textBoxAutoHoje.TabIndex = 14;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CalendarioIcone";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Restaurar,
+            this.Mensagem,
+            this.Fechar});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 133);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // Restaurar
+            // 
+            this.Restaurar.Name = "Restaurar";
+            this.Restaurar.Size = new System.Drawing.Size(240, 32);
+            this.Restaurar.Text = "Restaurar";
+            // 
+            // Mensagem
+            // 
+            this.Mensagem.Name = "Mensagem";
+            this.Mensagem.Size = new System.Drawing.Size(240, 32);
+            this.Mensagem.Text = "Mensagem";
+            // 
+            // Fechar
+            // 
+            this.Fechar.Name = "Fechar";
+            this.Fechar.Size = new System.Drawing.Size(240, 32);
+            this.Fechar.Text = "Fechar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 650);
+            this.Controls.Add(this.textBoxAutoHoje);
+            this.Controls.Add(this.textBoxAutoFinal);
+            this.Controls.Add(this.textBoxAutoInicial);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPegardata);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDataAtual);
+            this.Controls.Add(this.textBoxDataFinal);
+            this.Controls.Add(this.textBoxDatainicial);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBoxDatainicial;
+        private System.Windows.Forms.TextBox textBoxDataFinal;
+        private System.Windows.Forms.TextBox textBoxDataAtual;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnPegardata;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAutoInicial;
+        private System.Windows.Forms.TextBox textBoxAutoFinal;
+        private System.Windows.Forms.TextBox textBoxAutoHoje;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Restaurar;
+        private System.Windows.Forms.ToolStripMenuItem Mensagem;
+        private System.Windows.Forms.ToolStripMenuItem Fechar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calendario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxDatainicial.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBoxDataFinal.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+            textBoxDataAtual.Text = monthCalendar1.TodayDate.ToShortDateString();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBoxAutoInicial.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBoxDataFinal.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+            textBoxAutoHoje.Text = monthCalendar1.TodayDate.ToShortDateString();
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Name.ToString() == "Fechar")
+            {
+                this.Close();
+            }
+            else if (e.ClickedItem.ToString() == "Restaurar")
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else if (e.ClickedItem.Name.ToString() == "Mensagem")
+            {
+                MessageBox.Show("Voce esta lendo essa mensagem!!!");
+            }
+        }
+    }
+}
